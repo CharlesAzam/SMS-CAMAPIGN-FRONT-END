8769fb113bd9de47057458d66f112666d75c1170
Charles <Charles@azampay.com>
2019-11-12 09:44:36 +0300

    Added Channels, sub Category, tags, form

---
+++ src/app/components/create-channels/create-channels.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-channels',
+  templateUrl: './create-channels.component.html',
+  styleUrls: ['./create-channels.component.css']
+})
+export class CreateChannelsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
