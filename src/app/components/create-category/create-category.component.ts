ac9fecf6c2df3d5787ceee0f1df4e763489ff6ca
Charles <Charles@azampay.com>
2019-11-13 11:16:12 +0300

    Succesfully implemented test table logic with custom data

--- src/app/components/create-category/create-category.component.ts
+++ src/app/components/create-category/create-category.component.ts
@@ -1,6 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl} from '@angular/forms';
-import {MatTabGroup,MatTab} from '@angular/material'
+import {PeriodicElement} from './InterphacePeriodicElement'
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
+
+
+
 @Component({
   selector: 'app-create-category',
   templateUrl: './create-category.component.html',
@@ -9,11 +25,17 @@ import {MatTabGroup,MatTab} from '@angular/material'
 export class CreateCategoryComponent implements OnInit {
 
   constructor() { }
+  
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
+  
   toppings = new FormControl();
   options =new FormControl();
   toppingList: string[] = ['tag 1', 'tag 2', 'tag 3', 'tag 4', 'tag 5', 'tag 6'];
   toppingList2: string[] = ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5', 'Category 6'];
 
+  
+  
   isShow = true;
  
   toggleDisplay() {
