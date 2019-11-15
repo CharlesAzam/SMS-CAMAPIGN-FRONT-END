94dcee506d91d41c0fd22f26c1c211930811eaa2
Charles <Charles@azampay.com>
2019-11-15 17:43:10 +0300

    Refactored & Modified News Tag

---
+++ src/app/components/newsTags/news-tag/news-tag.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NewsTagComponent } from './news-tag.component';
+
+describe('NewsTagComponent', () => {
+  let component: NewsTagComponent;
+  let fixture: ComponentFixture<NewsTagComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NewsTagComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NewsTagComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
