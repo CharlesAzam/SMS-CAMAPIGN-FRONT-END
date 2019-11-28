0a47e3366cbd918896ec5386de39adad934eb8ef
Abhishek <abhisheknikam.pegasus@gmail.com>
2019-11-28 13:05:53 +0530

    Category Form Changes

--- src/app/components/mobileCategory/mobile category form/category-form.component.ts
+++ src/app/components/mobileCategory/mobile category form/category-form.component.ts
@@ -1,147 +1,194 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators, FormBuilder, FormGroup } from '@angular/forms';
-import { Router, ActivatedRoute } from '@angular/router';
-import { MobileCategories } from '../../../models/mobile-categories';
-import { SubCategoriesService } from 'src/app/services/sub.categories.service';
-import { BannerService } from '../../banner/banner.service';
-import { CategoriesService } from 'src/app/services/categories.service';
-import { Categories } from 'src/app/models/categories';
-import { LanguageService } from 'src/app/services/language.service';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormControl,
+  Validators,
+  FormBuilder,
+  FormGroup
+} from "@angular/forms";
+import { Router, ActivatedRoute } from "@angular/router";
+import { MobileCategories } from "../../../models/mobile-categories";
+import { SubCategoriesService } from "src/app/services/sub.categories.service";
+import { BannerService } from "../../banner/banner.service";
+import { CategoriesService } from "src/app/services/categories.service";
+import { Categories } from "src/app/models/categories";
+import { LanguageService } from "src/app/services/language.service";
 @Component({
-  selector: 'app-category-form',
-  templateUrl: './category-form.component.html',
-  styleUrls: ['./category-form.component.css']
+  selector: "app-category-form",
+  templateUrl: "./category-form.component.html",
+  styleUrls: ["./category-form.component.css"]
 })
 export class CategoryFormComponent implements OnInit {
-
-
   categoryForm: FormGroup = new FormGroup({
-    name: new FormControl('', [Validators.required]),
+    name: new FormControl("", [Validators.required]),
     // banner: new FormControl(''),
-    language: new FormControl('', [Validators.required]),
-    icon: new FormControl('', [Validators.required]),
-    type: new FormControl('', [Validators.required]),
-    subCategories: new FormControl(''),
-    priority: new FormControl('', [Validators.required]),
+    language: new FormControl("", [Validators.required]),
+    priority: new FormControl("", [Validators.required]),
+    icon: new FormControl("", [Validators.required]),
+    type: new FormControl("", [Validators.required]),
+    // subCategories: new FormControl(''),
     isHome: new FormControl(true, [Validators.required]),
+    isLeague: new FormControl(false, [Validators.required]),
+    leagueType: new FormControl(""),
+    leagueTypeImageThumb: new FormControl(""),
     status: new FormControl(true, [Validators.required])
-  })
+  });
 
   languages: any[] = [];
-  subCategories: any[] = []
-  banners: any[] = []
-  categoryModel: Categories
-  types: string[] = [
-    'RADIO',
-    'NEWS',
-    'TVGUIDE',
-    'VOD'
-  ]
+  subCategories: any[] = [];
+  banners: any[] = [];
+  categoryModel: Categories;
+  isUploading: boolean = false;
+  fileToUpload: any = null;
+  uploadImageURL: any = null;
+  types: string[] = ["RADIO", "NEWS", "TVGUIDE", "VOD"];
   icons: any[] = [
-    { key: 'ic_home', value: 'HOME' },
-    { key: 'ic_movie', value: 'MOVIES' },
-    { key: 'ic_sports', value: 'SPORTS' },
-    { key: 'ic_livetv', value: 'LIVETV' },
-    { key: 'ic_more', value: 'MORE' },
-  ]
+    { key: "ic_home", value: "HOME" },
+    { key: "ic_movie", value: "MOVIES" },
+    { key: "ic_sports", value: "SPORTS" },
+    { key: "ic_livetv", value: "LIVETV" },
+    { key: "ic_more", value: "MORE" }
+  ];
 
-
-  constructor(private router: Router,
+  constructor(
+    private router: Router,
     private activatedRoute: ActivatedRoute,
     private subCategoryService: SubCategoriesService,
     private bannerService: BannerService,
     private categoryService: CategoriesService,
-    private languageService: LanguageService) { }
+    private languageService: LanguageService
+  ) {}
 
   ngOnInit() {
-    this.getSubCategories();
+    // this.getSubCategories();
     // this.getBanners();
-    this.getLanguages()
+    this.getLanguages();
+
     this.activatedRoute.params.subscribe(params => {
-      if (params.id !== 'new') {
-        this.categoryService.findById(params.id).subscribe((response: any) => {
-          if (response.status === 200) {
-            console.log(response.data)
-            this.categoryModel = response.data[0];
-            this.categoryForm.setValue({
-              name: this.categoryModel.name ? this.categoryModel.name : '',
-              // banner: this.categoryModel.banner ? this.categoryModel.banner : '',
-              language: this.categoryModel.language ? this.categoryModel.language : '',
-              type: this.categoryModel.type ? this.categoryModel.type : '',
-              icon: this.categoryModel.icon ? this.categoryModel.icon : '',
-              subCategories: this.categoryModel.subCategories.map((subs) => { return subs.subCatID }) ? this.categoryModel.subCategories.map((subs) => { return subs.subCatID }) : [],
-              priority: this.categoryModel.priority ? this.categoryModel.priority : '',
-              isHome: String(this.categoryModel.isHome) ? String(this.categoryModel.isHome) : '',
-              status: String(this.categoryModel.status) ? String(this.categoryModel.status) : ''
-            })
-          }
-        }, error => console.error(error));
+      if (params.id !== "new") {
+        this.categoryService.findById(params.id).subscribe(
+          (response: any) => {
+            if (response.status === 200) {
+              console.log(response.data);
+              this.categoryModel = response.data[0];
+              this.categoryForm.setValue({
+                name: this.categoryModel.name ? this.categoryModel.name : "",
+                language: this.categoryModel.language
+                  ? this.categoryModel.language
+                  : "",
+                type: this.categoryModel.type ? this.categoryModel.type : "",
+                icon: this.categoryModel.icon ? this.categoryModel.icon : "",
+                priority: this.categoryModel.priority
+                  ? this.categoryModel.priority
+                  : "",
+                isHome: String(this.categoryModel.isHome)
+                  ? String(this.categoryModel.isHome)
+                  : "",
+                status: String(this.categoryModel.status)
+                  ? String(this.categoryModel.status)
+                  : "",
+
+                isLeague: String(this.categoryModel.isLeague)
+                  ? String(this.categoryModel.isLeague)
+                  : false,
+
+                leagueType: String(this.categoryModel.leagueType)
+                  ? String(this.categoryModel.leagueType)
+                  : "",
+
+                leagueTypeImageThumb: "" // this.categoryModel.leagueTypeImageThumb ? this.categoryModel.leagueTypeImageThumb : ""
+              });
+            }
+          },
+          error => console.error(error)
+        );
       }
     });
-
   }
 
   getBanners() {
-    this.bannerService.find().subscribe((response: any) => {
-      if (response.status === 200) {
-        this.banners = response.data
-      }
-    },
-      error => console.error(error))
+    this.bannerService.find().subscribe(
+      (response: any) => {
+        if (response.status === 200) {
+          this.banners = response.data;
+        }
+      },
+      error => console.error(error)
+    );
   }
 
   getSubCategories() {
-    this.subCategoryService.find().subscribe((response: any) => {
-      if (response.status === 200) {
-        console.log("responseresponseresponse", response)
-          ; this.subCategories = response.data
-      }
-    },
-      error => console.error(error))
+    this.subCategoryService.find().subscribe(
+      (response: any) => {
+        if (response.status === 200) {
+          this.subCategories = response.data;
+        }
+      },
+      error => console.error(error)
+    );
   }
 
   //Route To category List
   routeToCategoryList() {
-    this.router.navigate(['home/category']);
+    this.router.navigate(["home/category"]);
   }
 
   onSubmit() {
-    console.log(this.categoryForm.value)
-    if (this.categoryModel) {
-      Object.assign(this.categoryModel, this.categoryForm.value)
-
-      this.categoryService.update(this.categoryModel).subscribe((response: any) => {
-        if (response.status === 200)
-          this.routeToCategoryList()
-        else
-          console.log(response)
-      },
-        error => console.error(error))
-    } else {
-      this.categoryService.save(this.checkIfValueIsEmpty(this.categoryForm.value)).subscribe((response: any) => {
-        if (response.status === 200)
-          this.routeToCategoryList()
-        else
-          console.log(response)
-      },
-        error => console.error(error))
+    if (this.uploadImageURL) {
+      this.categoryForm.value["leagueTypeImageThumb"] = this.uploadImageURL;
     }
+    if (this.categoryModel) {
+      Object.assign(this.categoryModel, this.categoryForm.value);
 
-
+      this.categoryService.update(this.categoryModel).subscribe(
+        (response: any) => {
+          if (response.status === 200) this.routeToCategoryList();
+          else console.log(response);
+        },
+        error => console.error(error)
+      );
+    } else {
+      this.categoryService
+        .save(this.checkIfValueIsEmpty(this.categoryForm.value))
+        .subscribe(
+          (response: any) => {
+            if (response.status === 200) this.routeToCategoryList();
+            else console.log(response);
+          },
+          error => console.error(error)
+        );
+    }
   }
-
 
   getLanguages() {
-    this.languageService.list().subscribe((response) => {
-      if (response.status === 200) {
-        this.languages = response.data;
-      }
-    },
+    this.languageService.list().subscribe(
+      response => {
+        if (response.status === 200) {
+          this.languages = response.data;
+        }
+      },
       error => {
-        console.log("Error! ", error)
-      });
+        console.log("Error! ", error);
+      }
+    );
+  }
+  handelImageChange(files: FileList) {
+    this.fileToUpload = files.item(0);
+    this.fileToUpload.mimeType = this.fileToUpload.type;
+    this.uploadFileToActivity();
   }
 
+  uploadFileToActivity() {
+    this.isUploading = true;
+    this.categoryService.uploadUrl(this.fileToUpload).subscribe(
+      data => {
+        this.uploadImageURL = data.fileUrl ? data.fileUrl : "";
+        this.isUploading = false;
+      },
+      error => {
+        this.isUploading = false;
+      }
+    );
+  }
 
   checkIfValueIsEmpty(data) {
     for (let key in data) {
--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,67 +1,67 @@
-import { CategoriesFilter } from '../components/homepage/categories/categories-filter';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { CategoryFilter } from '../components/homepage/category/category-filter';
-import { Categories } from '../models/categories';
-import { API } from 'src/environments/environment';
+import { CategoriesFilter } from "../components/homepage/categories/categories-filter";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { CategoryFilter } from "../components/homepage/category/category-filter";
+import { Categories } from "../models/categories";
+import { API } from "src/environments/environment";
 
 @Injectable()
 export class CategoriesService {
-    constructor(private http: HttpClient) {
+  constructor(private http: HttpClient) {}
+  categoriesList: Categories[] = [];
+  findById(id: string): Observable<Categories> {
+    let url = API.BASE_URL + "/cms/category/" + id;
+    let headers = new HttpHeaders().set("Accept", "application/json");
+    return this.http.get<Categories>(url, { headers });
+  }
+  // load(filter: CategoriesFilter): void {
+  //     this.find().subscribe((result: any) => {
+  //         this.categoriesList = result.data;
+  //         console.log(this.categoriesList);
+  //     }, err => {
+  //         console.error('error loading', err);
+  //     });
+  // }
+  find(pageNumber?, size?): Observable<Categories[]> {
+    let url = API.BASE_URL + "/cms/category-list";
+    let headers = new HttpHeaders().set("Accept", "application/json");
+    if (pageNumber !== null || size !== null) {
+      let params = {
+        pageNumber: pageNumber,
+        size: size
+      };
+      return this.http.get<Categories[]>(url, { params, headers });
     }
-    categoriesList: Categories[] = [];
-    findById(id: string): Observable<Categories> {
-        let url = API.BASE_URL + '/cms/category/' + id;
-        let headers = new HttpHeaders()
-            .set('Accept', 'application/json');
-        return this.http.get<Categories>(url, { headers });
-    }
-    // load(filter: CategoriesFilter): void {
-    //     this.find().subscribe((result: any) => {
-    //         this.categoriesList = result.data;
-    //         console.log(this.categoriesList);
-    //     }, err => {
-    //         console.error('error loading', err);
-    //     });
-    // }
-    find(pageNumber?, size?): Observable<Categories[]> {
-        let url = API.BASE_URL + '/cms/category-list';
-        let headers = new HttpHeaders()
-            .set('Accept', 'application/json');
-        if (pageNumber !== null || size !== null) {
-            let params = {
-                "pageNumber": pageNumber,
-                "size": size
-            };
-            return this.http.get<Categories[]>(url, { params, headers });
-        }
-        return this.http.get<Categories[]>(url, { headers });
+    return this.http.get<Categories[]>(url, { headers });
+  }
+  save(entity: Categories): Observable<Categories> {
+    let url = API.BASE_URL + "/cms/category/create";
+    let headers = new HttpHeaders().set("Accept", "application/json");
+    return this.http.post<Categories>(url, entity, { headers });
+  }
 
-    }
-    save(entity: Categories): Observable<Categories> {
-        let url = API.BASE_URL + '/cms/category/create';
-        let headers = new HttpHeaders()
-            .set('Accept', 'application/json');
-        return this.http.post<Categories>(url, entity, { headers });
-    }
+  update(entity: Categories): Observable<Categories> {
+    let url = API.BASE_URL + `/cms/category/${entity._id}/update`;
+    let headers = new HttpHeaders().set("Accept", "application/json");
+    return this.http.put<Categories>(url, entity, { headers });
+  }
 
-    update(entity: Categories): Observable<Categories> {
-        let url = API.BASE_URL + `/cms/category/${entity._id}/update`;
-        let headers = new HttpHeaders()
-            .set('Accept', 'application/json');
-        return this.http.put<Categories>(url, entity, { headers });
-    }
+  delete(id: string): Observable<Categories> {
+    let url = API.BASE_URL + `/cms/category/${id}/`;
+    let headers = new HttpHeaders().set("Accept", "application/json");
+    return this.http.delete<Categories>(url, { headers });
+  }
 
-    delete(id: string): Observable<Categories> {
-        let url = API.BASE_URL + `/cms/category/${id}/`;
-        let headers = new HttpHeaders()
-            .set('Accept', 'application/json');
-        return this.http.delete<Categories>(url, { headers });
-    }
-
-    getCount() {
-        let url = API.BASE_URL + `/cms/count/category`;
-        return this.http.get(url);
-    }
+  getCount() {
+    let url = API.BASE_URL + `/cms/count/category`;
+    return this.http.get(url);
+  }
+  uploadUrl(fileToUpload: File): Observable<Object> {
+    let headers = new HttpHeaders();
+    const endpoint = API.BASE_URL + "/cms/upload-file";
+    const formData: FormData = new FormData();
+    formData.append("file", fileToUpload, fileToUpload.name);
+    return this.http.post(endpoint, formData, { headers });
+  }
 }
